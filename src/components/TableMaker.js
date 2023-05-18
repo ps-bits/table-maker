879e2bf33b6bb0cce16c6c26371ffb865a85b410
ps-bits <petri.words@gmail.com>
2023-05-18 17:35:37 +0200

    appearance and colors

--- src/components/TableMaker.js
+++ src/components/TableMaker.js
@@ -3,39 +3,51 @@ import React, { useState } from 'react';
 
 function TableMaker() {
   const [count, setCount] = useState(0);
-  const [rows, setRows] = useState(6);
-  const [cols, setCols] = useState(6);
+  const [rows, setRows] = useState(11);
+  const [cols, setCols] = useState(11);
 
   const changeRows = (e) => { setRows(e.currentTarget.value) }
   const changeCols = (e) => { setCols(e.currentTarget.value) }
 
-  calculate()
+  evaluateCode()
 
   return (
     <>
     
-    <div className="field is-horizontal">
-      <div className="field-label is-normal">
-        <label className="label">Size</label>
-      </div>
+    <div id="table-config" className="field is-horizontal">
+
       <div className="field-body">
 
         <div className="field">
-          <p className="control is-expanded">
-            <input className="input" type="text" placeholder="Name" value={rows} onChange={changeRows} />
+          <p className="control">
+            <button className="button is-success is-small" style={{padding: "4px"}} onClick={() => {  evaluateCode(); setCount(count + 1); }}>Make Table</button>
           </p>
         </div>
 
-        <div className="field">
+        <div className="field has-addons">
+          <div className="control">
+            <a className="button is-static is-small">
+              Rows
+            </a>
+          </div>
           <p className="control is-expanded">
-            <input className="input" type="text" placeholder="Name" value={cols} onChange={changeCols} />
+            <input className="input is-small" type="text" placeholder="Name" value={rows} onChange={changeRows} />
+          </p>
+        </div>
+
+        <div className="field has-addons">
+          <div className="control">
+            <a className="button is-static is-small">
+              Cols
+            </a>
+          </div>
+          <p className="control is-expanded">
+            <input className="input is-small" type="text" placeholder="Name" value={cols} onChange={changeCols} />
           </p>
         </div>
 
       </div>
     </div>
-
-    <button className="button is-dark" style={{padding: "4px"}} onClick={() => { setCount(count + 1); calculate(); }}>Make Table</button>
 
     { count >= 0 &&
       <XTable rows={rows} cols={cols} />
@@ -49,18 +61,27 @@ function TableMaker() {
 export default TableMaker;
 
 
-function calculate() {
+function evaluateCode() {
   var editor = window.ace.edit("editor");
-  let annotations = editor.getSession().getAnnotations()
+  let annotations = editor.getSession().getAnnotations();
 
   let code = editor.getValue(); // or session.getValue
 
   if (annotations.length == 0) {
-    console.log("no errors in code, doing eval")
-    window.eval(code)
-    window.foo(3)
+    console.log("no errors in code, doing eval");
+    window.eval(code);
   }
 
+}
+
+
+function calculateCell(row, col) {
+  return window.value(row, col);
+}
+
+
+function calculateCellColor(row, col) {
+  return window.cellColor(row, col);
 }
 
 
@@ -88,9 +109,9 @@ function XTable(props) {
 function TopRow(props) {
   const {cols} = props
   let cells = [];
-  cells.push(<td className="tableIndex">x</td>)
+  cells.push(<td key="-1" className="tableIndex">x</td>)
   for (let i=0; i<cols; i++) {
-    cells.push(<td className="tableIndex">{i}</td>)
+    cells.push(<td key={i} className="tableIndex">{i}</td>)
   }
   return (<tr>{cells}</tr>)
 }
@@ -99,7 +120,7 @@ function Row(props) {
   const {row, cols} = props
 
   let cells = [];
-  cells.push(<td className="tableIndex">{row}</td>)
+  cells.push(<td key="-1" className="tableIndex">{row}</td>)
 
   for (let i=0; i<cols; i++) {
     cells.push(<Cell key={i} row={row} col={i} />)
@@ -115,9 +136,10 @@ function Row(props) {
 
 function Cell(props) {
   const {row, col} = props
-  const value = window.foo(row, col)
+  const value = calculateCell(row, col);
+  const cellColor = calculateCellColor(row, col);
   return (
-    <td>
+    <td style={{backgroundColor: cellColor}}>
       {value}
     </td>
   )
